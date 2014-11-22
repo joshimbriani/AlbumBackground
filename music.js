5105ddcdb8f7fcb21370621703935a5fd3eecdd5
Josh Imbriani <joshimbriani@gmail.com>
2014-11-21 21:07:33 -0600

    Removed log

--- music.js
+++ music.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	$.get('http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + username + '&api_key=' + apiKeys.lastFM + '&format=json&limit=150', function(data) {
 		var counter = 0;
 		var dict = {};
-		console.log(data);
 		$.each(data.topalbums.album, function() {
 			if (!(this.image[2]['#text'] === '') && counter <= 150) {
 				if (!(this.image[2]['#text'] in dict)) {
